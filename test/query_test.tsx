aac67a42604b1ac69905a0e6a3f05d4e2dadec03
Nikolay Nemshilov <nemshilov@gmail.com>
2019-05-17 15:19:37 +1000

    Removing the decorated component support

--- test/query_test.tsx
+++ test/query_test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { graphql, Query } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { mock, render } from './helper';
 import { normalize } from '../src/utils';
@@ -24,11 +24,6 @@ const ToDos = ({ items, error, loading }: any) => {
   );
 };
 
-const DecoratedComponent = graphql(query)(
-  ({ data: { items = [], error, loading } }: any) =>
-    <ToDos items={items} error={error} loading={loading} />
-);
-
 export const QueryComponent = () =>
   <Query query={query}>
     {({ data: { items = [] } = {}, error, loading }: any) =>
@@ -37,30 +32,27 @@ export const QueryComponent = () =>
   </Query>;
 
 describe('query mocking', () => {
-  [DecoratedComponent, QueryComponent].forEach(Component => {
-  
-    describe(`${Component === DecoratedComponent ? 'decorated' : 'wrapped'} component`, () => {
-      it('handles mocked response', () => {
-        mock.expect(query).reply({
-          items: [
-            { id: '1', name: 'one' },
-            { id: '2', name: 'two' }
-          ]
-        });
-
-        expect(render(<Component />).html()).toEqual('<ul><li>one</li><li>two</li></ul>');
+  describe('Wrapped component', () => {
+    it('handles mocked response', () => {
+      mock.expect(query).reply({
+        items: [
+          { id: '1', name: 'one' },
+          { id: '2', name: 'two' }
+        ]
       });
 
-      it('allows to mock error states too', () => {
-        mock.expect(query).fail('everything is terrible');
+      expect(render(<QueryComponent />).html()).toEqual('<ul><li>one</li><li>two</li></ul>');
+    });
 
-        expect(render(<Component />).html()).toEqual('<div>GraphQL error: everything is terrible</div>');
-      });
+    it('allows to mock error states too', () => {
+      mock.expect(query).fail('everything is terrible');
 
-      it('allows to simulate loading state too', () => {
-        mock.expect(query).loading(true);
-        expect(render(<Component />).html()).toEqual('<div>Loading...</div>');
-      });
+      expect(render(<QueryComponent />).html()).toEqual('<div>GraphQL error: everything is terrible</div>');
+    });
+
+    it('allows to simulate loading state too', () => {
+      mock.expect(query).loading(true);
+      expect(render(<QueryComponent />).html()).toEqual('<div>Loading...</div>');
     });
   });
 
